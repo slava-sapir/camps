884f4f9189ec4a25fafc3d8b63f42c6c36564614
Dwayne <dwayne@reddingdesigns.com>
2024-07-18 10:42:38 -0400

    finish contact page blocks

---
+++ wp-content/themes/fcc/blocks/map/render.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Map Block Template.
+ *
+* @param array $block The block settings and attributes.
+* @param string $content The block inner HTML (empty).
+* @param bool $is_preview True during backend preview render.
+* @param int $post_id The post ID the block is rendering content against.
+*        This is either the post ID currently being displayed inside a query loop,
+*        or the post ID of the post hosting this block.
+* @param array $context The context provided to the block by the post or it's parent block.
+* 
+* @package Forest_Cliff_Camps
+*/
+?>
+
+<section id="location">
+    <div style="padding-top: <?= get_field('padding_top'); ?>px; padding-bottom: <?= get_field('padding_bottom'); ?>px;">
+        <?= get_field('embed_code'); ?>
+    </div>
+</section>
