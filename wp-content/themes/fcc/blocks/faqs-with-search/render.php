611c595a77b558edee3206399d39bf3571c486a2
Dwayne Froese <dwayne@reddingdesigns.com>
2024-08-07 10:40:22 -0400

    finish Day Camp landing page

--- wp-content/themes/fcc/blocks/faqs-with-search/render.php
+++ wp-content/themes/fcc/blocks/faqs-with-search/render.php
@@ -29,7 +29,7 @@ $displayed_category = get_field('displayed_category');
                     <div class="position-relative">
                         <label for="s" class="d-block mb-5"><small>Search Your Question</small></label>
                         <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Input keywords" class="form-control pe-40" />
-                        
+                        <input type="hidden" name="post_type" value="faq"/>
                         <button type="submit" class="position-absolute end-0 border-0 outline-0 bg-transparent">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none"><path d="M10 18.2793C11.775 18.2789 13.4988 17.6847 14.897 16.5913L19.293 20.9873L20.707 19.5733L16.311 15.1773C17.405 13.779 17.9996 12.0547 18 10.2793C18 5.8683 14.411 2.2793 10 2.2793C5.589 2.2793 2 5.8683 2 10.2793C2 14.6903 5.589 18.2793 10 18.2793ZM10 4.2793C13.309 4.2793 16 6.9703 16 10.2793C16 13.5883 13.309 16.2793 10 16.2793C6.691 16.2793 4 13.5883 4 10.2793C4 6.9703 6.691 4.2793 10 4.2793Z" fill="#58616B"/><path d="M11.4121 8.86538C11.7911 9.24538 12.0001 9.74738 12.0001 10.2794H14.0001C14.001 9.75381 13.8977 9.23328 13.6961 8.74787C13.4946 8.26246 13.1989 7.82181 12.8261 7.45138C11.3121 5.93938 8.68707 5.93938 7.17407 7.45138L8.58607 8.86738C9.34607 8.10938 10.6561 8.11138 11.4121 8.86538Z" fill="#58616B"/></svg>
                         </button>
--- wp-content/themes/fcc/blocks/info-cards/render.php
+++ wp-content/themes/fcc/blocks/info-cards/render.php
@@ -17,7 +17,7 @@ $anchor = isset($block['anchor']) ? $block['anchor'] : '';
 $classes = isset($block['className']) ? $block['className'] : '';
 ?>
 
-<section class="info-cards tile-shadow <?= esc_attr($classes) ?>" id="<?= esc_attr($anchor) ?>" style="padding-top: <?= get_field('padding_top'); ?>px; padding-bottom: <?= get_field('padding_bottom'); ?>px;">
+<section class="info-cards <?= esc_attr($classes) ?>" id="<?= esc_attr($anchor) ?>" style="padding-top: <?= get_field('padding_top'); ?>px; padding-bottom: <?= get_field('padding_bottom'); ?>px;">
     <div class="container container-ed">
         <h2 class="text-center"><?= get_field('heading'); ?></h2>
         <div class="row gy-50 gx-md-50">
@@ -26,7 +26,7 @@ $classes = isset($block['className']) ? $block['className'] : '';
                 $image = get_sub_field('image');
                 $link = get_sub_field('link'); ?>
                 <div class="col-md-6 col-lg-4 d-flex">
-                    <div class="info-card position-relative d-flex flex-column align-self-stretch w-100">
+                    <div class="info-card rounded fcc-shadow position-relative d-flex flex-column align-self-stretch w-100">
                         <?= wp_get_attachment_image( $image['id'], 'full', false, ['class' => 'img-fluid text-center'] ); ?>
                         <div class="info-card-text tile-shadow pt-40 px-20">
                             <h3 class="h4 text-center text-white"><?= $title ?></h3>
