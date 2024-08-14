46ef659b279551f969ae4451a7f38d16ac764bc1
Slava <slava@reddingdesigns.com>
2024-08-14 16:20:23 -0400

    experience-carousel

--- wp-content/themes/fcc/resources/js/theme.js
+++ wp-content/themes/fcc/resources/js/theme.js
@@ -2,9 +2,12 @@
 import 'bootstrap/js/dist/offcanvas';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/modal';
+import "bootstrap/js/dist/tab"; 
 // import 'bootstrap/js/dist/tooltip';
 
+
 import '../../blocks/camp-reviews/camp-reviews';
+import "../../blocks/experience-carousel/experience-carousel";
 
 document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
 
