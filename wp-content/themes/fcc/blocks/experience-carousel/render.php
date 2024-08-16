4c9bd191b9f6062fc31b52a3a415627107b7b7c8
Slava <slava@reddingdesigns.com>
2024-08-16 10:46:26 -0400

    adding color theme feature

--- wp-content/themes/fcc/blocks/experience-carousel/render.php
+++ wp-content/themes/fcc/blocks/experience-carousel/render.php
@@ -14,16 +14,24 @@
 */
 ?>
 
-<section id="experience-carousel" class="bg-gold bg-secondary-gradient mb-50">
+<!-- <section id="experience-carousel" class="bg-gold bg-secondary-gradient"> -->
+  <section id="experience-carousel" >
+<?php 
+  $id = get_the_ID();
+  $theme_color = get_field('theme_colour', $id);
+?>
+<?php 
+  if($theme_color != 'green-theme') : ?>
+  <img src="<?= esc_url(get_template_directory_uri() . '/resources/images/main-theme/shapes/white-top-splash.png'); ?>" alt="white-top-splash" class="img-fluid w-100 mt-n1">
+<?php endif; ?>
 
-    <img src="<?= esc_url(get_template_directory_uri() . '/resources/images/main-theme/shapes/white-top-splash.png'); ?>" alt="white-top-splash" class="img-fluid w-100 mt-n1">
-    <div class="bottom-splash hp-1400 hp-sm-1000 hp-md-1200 hp-xl-800 position-relative">
+    <div class="hp-1400 hp-sm-1000 hp-md-1200 hp-xl-800 position-relative">
 
       <div class="d-flex flex-column-reverse flex-xl-row align-items-center">
 
-        <div class="d-flex align-item-center justify-content-center w-100 w-xl-50 border-bottom border-1 border-white">
+        <div class="d-flex align-item-center justify-content-center w-100 w-xl-50 border-bottom border-1 <?= $theme_color == 'green-theme' ? 'border-grey600' : 'border-white' ?>">
             <div class="d-flex flex-column gap-30 gap-md-80">
-             <h2 class="text-white text-center"><?= get_field('main_title'); ?></h2>
+             <h2 class="text-center <?= $theme_color == 'green-theme' ? 'text-grey600' : 'text-white '; ?>"><?= get_field('main_title'); ?></h2>
               <div class="nav nav-tabs d-flex flex-column flex-sm-row justify-content-end align-items-center gap-5 gap-md-20" id="nav-tab" role="tablist">
                     <?php
                     $count = 0;
@@ -44,21 +52,21 @@
                             aria-controls="nav-<?php echo $index; ?>"
                             aria-selected="false">
                             <div class="click-event mb-n16 flex-column justify-content-center align-items-center <?= $count == 1 ? 'd-flex' : 'd-none'; ?>">
-                                <div class="bordered-big-icon p-20 border border-2 border-white rounded-circle d-flex align-items-center justify-content-center">
+                                <div class="bordered-big-icon p-20 border border-2 <?= $theme_color == 'green-theme' ? 'border-grey600' : 'border-white '; ?> rounded-circle d-flex align-items-center justify-content-center">
                                     <?= wp_get_attachment_image( $big_icon['id'], 'full', false, ['class' => 'img-fluid'] ); ?>
                                 </div>
-                                <div class="fs-6 fw-semibold text-white txt pt-10 pb-5"><?= $experience_title ?></div>
+                                <div class="fs-6 fw-semibold <?= $theme_color == 'green-theme' ? 'text-grey600' : 'text-white '; ?> txt pt-10 pb-5"><?= $experience_title ?></div>
                                 <div class="d-flex flex-column align-items-center justify-content-center">
-                                  <div class="wp-2 hp-20 bg-white"></div> 
-                                  <div class="bordered-ball border border-2 border-white rounded-circle d-flex align-items-center justify-content-center wp-20 hp-20">
-                                    <div class="bg-white rounded-circle wp-10 hp-10"></div>
+                                  <div class="wp-2 hp-20 <?= $theme_color == 'green-theme' ? 'bg-grey600' : 'bg-white '; ?>"></div> 
+                                  <div class="bordered-ball border border-2 <?= $theme_color == 'green-theme' ? 'border-grey600' : 'border-white '; ?> rounded-circle d-flex align-items-center justify-content-center wp-20 hp-20">
+                                    <div class="<?= $theme_color == 'green-theme' ? 'bg-grey600' : 'bg-white '; ?> rounded-circle wp-10 hp-10"></div>
                                   </div>
                                 </div>
                             </div>
                             <div class="hover-event mb-n12 d-flex flex-column gap-10 justify-content-center align-items-center <?= $count == 1 ? 'd-none' : 'd-flex'; ?>">
                                 <?= wp_get_attachment_image( $small_icon['id'], 'full', false, ['class' => 'img-fluid small-icon'] ); ?>
-                                <p class="fs-6 fw-semibold text-white txt"><?= $experience_title ?></p>
-                                <div class="ball bg-white rounded-circle wp-10 hp-10"></div>
+                                <p class="fs-6 fw-semibold text-white txt <?= $theme_color == 'green-theme' ? 'text-grey600' : 'text-white '; ?>"><?= $experience_title ?></p>
+                                <div class="ball <?= $theme_color == 'green-theme' ? 'bg-grey600' : 'bg-white '; ?> rounded-circle wp-10 hp-10"></div>
                             </div>
                       </a>
                 
@@ -81,7 +89,7 @@
               ?>
 
               <div class="tab-pane fade <?php echo $count === 1 ? 'show active' : ''; ?>" id="nav-<?php echo $index; ?>" role="tabpanel" aria-labelledby="nav-<?php echo $index; ?>-tab" tabindex="0">
-                <div class="d-flex flex-column bg-white rounded-circle border-20 border border-white wp-420 wp-md-520 wp-lg-671 hp-420 hp-md-520 hp-lg-671" >
+                <div class="rounded-container d-flex flex-column bg-white rounded-circle border-20 border border-white wp-420 wp-md-520 wp-lg-671 hp-420 hp-md-520 hp-lg-671" >
           
                   <?= wp_get_attachment_image( $experience_image['id'], 'circled-image', false, ['class' => 'img-fluid rounded-top-5'] ); ?>
                   <div class="d-flex flex-column gap-5 justify-content-center align-items-center">
@@ -97,7 +105,11 @@
 
       </div>
 
+      <?php 
+       if($theme_color != 'green-theme') : ?>
       <img src="<?= esc_url(get_template_directory_uri() . '/resources/images/main-theme/shapes/white-top-splash.png'); ?>" alt="white-top-splash-rotate" class="img-fluid w-100 position-absolute start-0 bottom-0 transform-rotateY-rotateX mb-n1">
+      <?php endif; ?>
+     
     </div>
 
 </section>
