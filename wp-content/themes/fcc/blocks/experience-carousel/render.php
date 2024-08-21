a2abba6770bb58a7bbad07818b011026fbb5714d
Slava <slava@reddingdesigns.com>
2024-08-21 15:31:28 -0400

    second version

--- wp-content/themes/fcc/blocks/experience-carousel/render.php
+++ wp-content/themes/fcc/blocks/experience-carousel/render.php
@@ -41,7 +41,7 @@
                      $index = get_row_index();
                      $count++;
                     ?>
-                      <a href="javascript:void(0);" role="presentation" class="presentation hp-171 nav-link d-flex justify-content-center align-items-end position-relative <?= $count == 1 ? 'active' : ''; ?>"
+                      <a href="javascript:void(0);" role="presentation" class="presentation hp-171 d-flex justify-content-center align-items-end nav-link position-relative <?= $count == 1 ? 'active' : ''; ?>"
                             id="nav-<?php echo $index; ?>-tab"
                             data-bs-toggle="tab"
                             data-bs-target="#nav-<?php echo $index; ?>"
@@ -49,22 +49,17 @@
                             role="tab"
                             aria-controls="nav-<?php echo $index; ?>"
                             aria-selected="false">
-                            <div class="click-event mb-n16 flex-column justify-content-center align-items-center <?= $count == 1 ? 'd-flex' : 'd-none'; ?>">
-                                <div class="bordered-big-icon p-20 border border-2 <?= $theme_color == 'green-theme' ? 'border-grey600' : 'border-white '; ?> rounded-circle d-flex align-items-center justify-content-center">
-                                    <?= wp_get_attachment_image( $big_icon['id'], 'full', false, ['class' => 'img-fluid'] ); ?>
+                            <div class="tab_class mb-n16 flex-column justify-content-center align-items-center">
+                                <div class="bordered-icon rounded-circle p-20 <?= $theme_color == 'green-theme' ? 'border-grey600' : 'border-white '; ?> text-center">
+                                    <?= wp_get_attachment_image( $small_icon['id'], 'full', false, ['class' => 'img-fluid'] ); ?>
                                 </div>
-                                <div class="fs-6 fw-semibold <?= $theme_color == 'green-theme' ? 'text-grey600' : 'text-white '; ?> txt pt-10 pb-5"><?= $experience_title ?></div>
-                                <div class="d-flex flex-column align-items-center justify-content-center">
-                                  <div class="wp-2 hp-20 <?= $theme_color == 'green-theme' ? 'bg-grey600' : 'bg-white '; ?>"></div> 
-                                  <div class="bordered-ball border border-2 <?= $theme_color == 'green-theme' ? 'border-grey600' : 'border-white '; ?> rounded-circle d-flex align-items-center justify-content-center wp-20 hp-20">
-                                    <div class="<?= $theme_color == 'green-theme' ? 'bg-grey600' : 'bg-white '; ?> rounded-circle wp-10 hp-10"></div>
+                                <div class="txt fs-6 fw-semibold <?= $theme_color == 'green-theme' ? 'text-grey600' : 'text-white '; ?> pt-10 pb-5"><?= $experience_title ?></div>
+                                <div class="stick-ball d-flex flex-column align-items-center justify-content-center">
+                                  <div class="stick wp-2 hp-20 <?= $theme_color == 'green-theme' ? 'bg-grey600' : 'bg-white '; ?>"></div> 
+                                  <div class="bordered-ball p-3 rounded-circle border_opacity">
+                                     <div class="ball rounded-circle <?= $theme_color == 'green-theme' ? 'bg-grey600' : 'bg-white'; ?> wp-10 hp-10"></div>
                                   </div>
                                 </div>
-                            </div>
-                            <div class="hover-event mb-n12 d-flex flex-column gap-10 justify-content-center align-items-center <?= $count == 1 ? 'd-none' : 'd-flex'; ?>">
-                                <?= wp_get_attachment_image( $small_icon['id'], 'full', false, ['class' => 'img-fluid small-icon'] ); ?>
-                                <p class="fs-6 fw-semibold text-white txt <?= $theme_color == 'green-theme' ? 'text-grey600' : 'text-white '; ?>"><?= $experience_title ?></p>
-                                <div class="ball <?= $theme_color == 'green-theme' ? 'bg-grey600' : 'bg-white '; ?> rounded-circle wp-10 hp-10"></div>
                             </div>
                       </a>
                 
@@ -90,7 +85,7 @@
                 <div class="rounded-container d-flex flex-column bg-white rounded-circle border-20 border border-white wp-420 wp-md-520 wp-lg-671 hp-420 hp-md-520 hp-lg-671" >
                   <?= wp_get_attachment_image( $experience_image['id'], 'circled-image', false, ['class' => 'img-fluid rounded-top-5'] ); ?>
                   <div class="d-flex flex-column gap-5 justify-content-center align-items-center">
-                    <div class=" mt-n30 bg-white rounded-circle p-10">
+                    <div class="mt-n30 bg-white rounded-circle p-10">
                     <?= wp_get_attachment_image( $yellow_icon['id'], 'full', false, ['class' => 'img-fluid'] ); ?>
                     </div>
                     <p class="fs-4 fw-normal text-grey600 m-0"><?php echo $experience_title; ?></p>
