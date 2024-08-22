6d074bd8dbe5181295a57eda605fca57d37ad5e2
Slava <slava@reddingdesigns.com>
2024-08-22 15:11:39 -0400

    initial commit

---
+++ wp-content/themes/fcc/blocks/locations/render.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Rates & Dates Block Template.
+ *
+* @param array $block The block settings and attributes.
+* @param string $content The block inner HTML (empty).
+* @param bool $is_preview True during backend preview render.
+* @param int $post_id The post ID the block is rendering content against.
+*        This is either the post ID currently being displayed inside a query loop,
+*        or the post ID of the post hosting this block.
+* @param array $context The context provided to the block by the post or it's parent block.
+* 
+* @package Forest_Cliff_Camps
+*/
+?>
+
+<section id="rates-dates">
+
+ <img src="<?= esc_url(get_template_directory_uri() . '/resources/images/main-theme/shapes/white-top-splash.png'); ?>" alt="white-top-splash" class="img-fluid w-100 mt-n1">
+
+    <div class="container">
+                
+      
+    
+
+    </div>
+    
+</section>
